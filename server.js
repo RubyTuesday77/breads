2930ba6d0d486c83de6b87ff8b6c3eca1ea08f74
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-09-26 19:08:54 -0400

    sending index and show  data

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 
 // ROUTES
@@ -25,6 +27,10 @@ const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
--- views/index.jsx
+++ views/index.jsx
@@ -1,12 +1,11 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/default')
 
-function Index ({breads}) {
+function Index({breads}) {
     return (
         <Default>
             <h2>Index Page</h2>
             {/* <p>I have {breads[0].name} bread!</p> */}
-            {/* This is a JSX comment. */}
             <ul>
                 {
                     breads.map((bread, index) => {
