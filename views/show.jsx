204b84ce11aec81686c4a1afa1e20272718e50e4
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-09-28 21:20:42 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -17,10 +17,12 @@ function Show({ bread, index }) {
                 have gluten.
             </p>
             <img src={ bread.image } alt={ bread.name } />
-            <form action={ `/breads/${ index }?_method=DELETE` } method="POST">
+            <a href={ `/breads/${index}/edit` }>
+                <button>Edit</button>
+            </a>
+            <form action={ `/breads/${index}?_method=DELETE` } method="POST">
                 <input type='submit' value="DELETE"/>
             </form>
-            <li><a href="/breads">Go home</a></li>
         </Default>
     )
 }
