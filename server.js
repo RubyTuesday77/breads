bd381ba4cae9719a6c5d5810fb92d5a8b1ec96f4
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-10-15 13:50:02 -0400

    fixed mongodb_uri

--- server.js
+++ server.js
@@ -10,17 +10,9 @@ const PORT = process.env.PORT
 const app = express()
 
 // Mongoose:
-const MONGO_URI = process.env.MONGO_URI
-mongoose.connect(
-  MONGO_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  () => {
-    console.log(`connected to mongo: ${MONGO_URI}`)
-  }
-)
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+  () => {console.log('connected to : ', process.env.MONGO_URI)}
+  )
 
 // MIDDLEWARE:
 app.set('views', __dirname + '/views')
